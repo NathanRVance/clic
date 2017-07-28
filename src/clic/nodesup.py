17b3b6fdc3c1c45ec8a001f8f295f5a601a6cfb9
Nathan Vance <natervance@gmail.com>
2017-07-28 11:47:00 -0400

    testing nodesup

--- src/clic/nodesup.py
+++ src/clic/nodesup.py
@@ -6,7 +6,12 @@ from clic import pssh
 from clic import cloud as api
 cloud = api.getCloud()
 
-def responds(user, nameRegex = None):
+def responds(user=None, nameRegex=None):
+    if user is None:
+        import configparser
+        config = configparser.ConfigParser()
+        config.read('/etc/clic/clic.conf')
+        user = config['Daemon']['user']
     if nameRegex is None:
         nameRegex = re.compile('.*')
     return [node for node in all(True) if nameRegex.search(node) and pssh.canConnect(user, user, node)]
@@ -19,7 +24,7 @@ def main():
     parser = argparse.ArgumentParser(description='Obtain a list of nodes that are running')
     from clic import version
     parser.add_argument('-v', '--version', action='version', version=version.__version__)
-    parser.add_argument('-r', '--responds', metavar='USER', nargs=1, help='include only nodes that respond to ssh connections')
+    parser.add_argument('-r', '--responds', metavar='USER', nargs='?', help='include only nodes that respond to ssh connections')
     args = parser.parse_args()
 
     if args.responds:
