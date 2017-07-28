52ebbf5973ed4bfd64dc1aef3638c5abb8402873
Nathan Vance <natervance@gmail.com>
2017-07-28 08:59:10 -0400

    bug fixes

--- src/clic/queue.py
+++ src/clic/queue.py
@@ -46,10 +46,10 @@ class slurm(abstract_queue):
             with open('{}/slurm.conf'.format(self.slurmDir)) as f:
                 data = f.read()
             for partition in partitions:
-                if not re.search('={0}-{1}-\[0-\d+\] '.format(namescheme, partition.name), data):
+                if not re.search('={0}-{1}-\[0-\d+\] '.format(self.namescheme, partition.name), data):
                     # RealMemory, TmpDisk in mb
-                    data += 'NodeName={0}-{1}-[0-0] CPUs={2} TmpDisk={3} RealMemory={4} State=UNKNOWN\n'.format(namescheme, partition.name, partition.cpus, partition.disk * 1024, partition.realMem * 1024)
-                    data += 'PartitionName={1} Nodes={0}-{1}-[0-0] MaxTime=UNLIMITED State=UP\n'.format(namescheme, partition.name)
+                    data += 'NodeName={0}-{1}-[0-0] CPUs={2} TmpDisk={3} RealMemory={4} State=UNKNOWN\n'.format(self.namescheme, partition.name, partition.cpus, partition.disk * 1024, partition.realMem * 1024)
+                    data += 'PartitionName={1} Nodes={0}-{1}-[0-0] MaxTime=UNLIMITED State=UP\n'.format(self.namescheme, partition.name)
             with open('{}/slurm.conf'.format(self.slurmDir), 'w') as f:
                 f.write(data)
     
